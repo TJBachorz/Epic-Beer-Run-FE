f8a613611a05317ef61634eb7efa521a4095f80e
TJBachorz <TJBachorz@gmail.com>
2020-10-23 10:39:18 -0600

    fixes css on road trip

--- src/components/DestinationCard.js
+++ src/components/DestinationCard.js
@@ -3,21 +3,21 @@ import React from 'react'
 import { Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 
+
 const DestinationInfo = styled.div`
-    display: flex
+    background-image: url("https://cdnimg.webstaurantstore.com/images/products/large/202832/790435.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    flex-flow: row wrap;
     justify-content:center;
     align-items:center;
     width: 200px;
     height: 100px;
-    white-space: nowrap;
-    overflow: auto;
-    border: 2px solid black;
+    border: 2px solid gray;
     border-radius: 15px;
     background-color: white;
     margin: 1px;
-    overflow-wrap: break-word;
-    word-wrap: break-word;
-    hyphens: auto;
     text-align:center;
 `;
 
@@ -39,17 +39,17 @@ export default function DestinationCard({coordinate, index}) {
                     ref={provided.innerRef}
                 >
                     <div className="marker-info">
-                        <h5 className="destination-name">
-                            <a href={coordinate[2].website_url}
-                                target="_blank" 
-                                rel="noopener noreferrer"
-                            >
-                                {coordinate[2].name}
-                            </a>
-                        </h5>
-                        <p className="destination-location">
-                                {`${coordinate[2].city}, ${coordinate[2].state}`}
-                        </p>
+                    <p className="destination-name">
+                        <a href={coordinate[2].website_url}
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                        >
+                            {coordinate[2].name}
+                        </a>
+                    </p>
+                    <p className="destination-location">
+                            {`${coordinate[2].city}, ${coordinate[2].state}`}
+                    </p>
                     </div>
                 </DestinationInfo>
             )}
--- src/components/DestinationCardContainer.js
+++ src/components/DestinationCardContainer.js
@@ -10,10 +10,8 @@ const DestinationList = styled.div`
     display: flex;
     flex-direction: row;
     overflow: auto;
-    white-space: nowrap;
-    width: 70%;
+    width: 850px;
     margin: 20px;
-    border-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)
 `;
 
 export default function DestinationCardContainer({coordinates}) {
